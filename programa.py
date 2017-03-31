05fb8e60c0fb19f5588f2b1d8522092dfbda9f1f
Veguinho <rafaelvr@al.insper.edu.br>
2017-03-31 08:50:50 -0300

    Update programa.py

--- programa.py
+++ programa.py
@@ -34,8 +34,8 @@ while 1:
 	elif start == "1":
 		clear()
 		print("Vamos começar!")
-		clear()
 		time.sleep(3)
+		clear()
 		for i in range(5,0,-1):
 			print(i)
 			time.sleep(1)
@@ -45,6 +45,7 @@ while 1:
 		print("Você precisa digitar ou 0 ou 1!")
 		time.sleep(3)
 		clear()
+
 		
 
 
