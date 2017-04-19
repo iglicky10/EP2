36d1346b439de273055aacc2c9c04ec847c0ca65
Veguinho <rafaelvr@al.insper.edu.br>
2017-04-19 16:38:58 -0300

    Primeiro jogo funcional basico_v2

--- programa.py
+++ programa.py
@@ -1,31 +1,125 @@
+
 #INSPERMON
 import os
 import time
 import sys
 import json
+import random
 
 clear = lambda: os.system('cls')
 clear()
 
-logo = """
-                                                                                                                        
-       `:',` `:'',`   ,':`   :''';`  .:''''',`  .:'''''', .:''''';.   `:'';.     .;'':`    .;''''.    .:'',`   :':`          
-       `'@;` .+@@+.   ;@'. `:#@@@#'. ,+@@@@@+;. ,+@@@@@@:`.+@@@@@@+.  ,+@@@:`    ;#@@+.  `:+#@@@@+;.  .+@@+.   '@'.          
-       `'@;` .+#@@:`  ;@'. :#@;,,;+, ,#@;,,+@#: ,#@',,,,. .+@',,;#@'. ,#@##'.   .+##@+.  :#@':,,+@@:` .+##@:`  ;@'.          
-       `'@;` .+#'+'.  ;@'. '@+.  `,` ,#@,  ,#@;`,#@:      .+@:  `'@#, ,#@'+#,   ,#++#+. .+@'.   .+@+. .+#'+'.  ;@'.          
-       `'@;` .+#,'#,  ;@'. '@#:`     ,#@,  .+@;`,#@,      .+@:   ;@#, ,#@:'@;`  ;@;'@+. ;##:     ;#@, .+#,'#,  ;@'.          
-       `'@;` .+#,,#'. ;@'. ,#@+':`   ,#@,  :#@: ,#@+''':` .+@:  .+@'. ,#@,;#+. `'#,'@+. '@+,     :#@: .+#,,#'. ;@'.          
-       `'@;` .+#,`'#, ;@'. `,#@@#'.  ,#@+''#@+. ,#@@@@@+. .+@+''+#+,  :#@,,+#, :#+.;@+. '@+.     ,#@: .+#,`'#, ;@'.          
-       `'@;` .+#, ,#'.;@'.  `.:+@@+. ,#@@@@@;.` ,#@;,,,.` .+@@@@@+,`  ,#@,`'@;`'@;`;@+. '@+.     ,#@: .+#, ,#'.;@'.          
-       `'@;` .+#, `'@,;@'.    `,'#@;`,#@;,,,`   ,#@,      .+@',;##:`  ,#@, ;@+:+@, '@+. '@#,     :#@: .+#, `'#,'@'.          
-       `'@;` .+#,  ,#;;#'.      .+@;`,#@,       ,#@,      .+@: `;@#,  ,#@, ,+@##'. '@+. ;#@,    `;@#, .+#,  ,#;;#'.          
-       `'@;` .+#,  `'#+#'.`:.   ,+@;`,#@,       ,#@,      .+@:  .+@;` ,#@, `'@@@:  ;@+. .+@+.   :#@;` .+#,  `'#+#'.          
-       `'@;` .+#,   ,#@@'.`'+'''+@+, ,#@,       ,#@+'''',`.+@:  `'@#, ,#@,  ;@@#,  ;@+.  :#@+'''#@'.  .+#,   ,##@'.          
-       `'@;` .+#,   `'@@;``,+@@@@;.` :#@,       ,+@@@@@@;`.+@:`  ,#@'`:#@,  ,#@'`  '@#,  `,+@@@@#;.   .+@,   `'@@'`          
-       `.,.  `,,`    .,,.   .:,,,`   `,,`       `,,,,,,,. `,,.   `,,,``,,`  `,,.   .,,`    .,,,,,`    `,,`    .,,. 
+#CLASSES E FUNÇÕES
+class Alunos:
+	def __init__(self,nome,ataque,defesa,vida,lvl):
+		self.ataque = ataque
+		self.defesa = defesa
+		self.vida = vida
+		self.nome = nome
+		self.lvl = lvl
 
-       Bem vindos ao Inspermon! Um jogo completamente inovador, nunca visto antes, onde você anda por um mundo virtual e encontra os Inspermons! 
-       *** JOGUE COM O O CONSOLE EM TELA CHEIA *** 
+def batalhar(player,oponente,nomeSeuAluno):
+	if player == "alunoMecatronica":
+		player = alunoMecatronica
+	elif player == "alunoMecanica":
+		player = alunoMecanica
+	elif player == "alunoComputacao":
+		player = alunoComputacao
+	clear()
+	print("Você encontrou um {} ! O level dele é: {}".format(oponente.nome,oponente.lvl))
+	time.sleep(4)
+
+	if oponente.lvl > player.lvl:
+		print("ATENÇÃO! O LEVEL DO ADVERSÁRIO É MAIOR DO QUE O SEU!")
+		time.sleep(2)
+
+	vidaOponente = oponente.vida
+	suaVida = player.vida
+
+	print("Prepare-se para a batalha!")
+	time.sleep(2)
+
+	#LOOP DA BATALHA
+	while suaVida and vidaOponente > 0:
+		clear()
+		print("Pontos de vida de {}: {}".format(nomeSeuAluno,suaVida))
+		print("Pontos de vida do {}: {}".format(oponente.nome,vidaOponente))
+
+		atacarChoice = input("Você quer atacar (a) ou tentar fugir (f)? ")
+
+		if atacarChoice == "f" or atacarChoice == "F":
+			clear()
+			print("Você fugiu da batalha com êxito!")
+			time.sleep(2)
+			return
+
+		elif atacarChoice == "a" or atacarChoice == "A":
+			clear()
+			vidaOponente = vidaOponente - (player.ataque - oponente.defesa)
+			suaVida = suaVida - (oponente.ataque - player.defesa)
+			print("{} atacou e inflingiu {} de dano!".format(nomeSeuAluno,player.ataque-oponente.defesa))
+			time.sleep(2)
+			print("O {} inimigo atacou e inflingiu {} de dano!".format(oponente.nome,oponente.ataque-player.defesa))
+			time.sleep(3)
+
+		else:
+			print("Você precisa digitar (a) para atacar ou (f) para fugir!")
+
+	if suaVida <= 0:
+		clear()
+		print("Você perdeu a batalha...")
+		time.sleep(1)
+		print("Mas não se preocupe, como esse jogo foi feito para se divertir, o seu Aluno já se recuperou e está pronto para continuar com você em sua jornada!")
+		time.sleep(5)
+		return
+
+	elif vidaOponente <= 0: 
+		clear()
+		print("Parabéns! Você venceu a batalha!")
+		time.sleep(2)
+		print("Continue jogando para treinar seu aluno!")
+		time.sleep(3)
+		return
+
+
+#DADOS
+###INICIAIS
+alunoMecatronica = Alunos("Name",10,5,30,1)
+alunoMecanica = Alunos("Name",10,5,30,1)
+alunoComputacao = Alunos("Name",10,5,30,1)
+####NPCs
+bixoADM = Alunos("Bixo de ADM",4,3,25,1)
+bixoECONO = Alunos("Bixo de ECONOMIA",7,2,22,1)
+bixoMECATRONICA = Alunos("Bixo de MECATRÔNICA",7,4,25,1)
+bixoMECANICA = Alunos("Bixo de MECÂNICA",9,3,22,1)
+bixoCOMPUTACAO = Alunos("Bixo de COMPUTAÇÃO",6,4,30,1)
+veteranoADM = Alunos("Veterano de ADM",10,7,40,2)
+veteranoECONO = Alunos("Veterano de ECONOMIA",15,4,36,2)
+listaNPC = [bixoADM,bixoECONO,bixoMECATRONICA,bixoMECANICA,bixoCOMPUTACAO,veteranoADM,veteranoECONO]
+
+
+
+logo = """
+
+	   `:',` `:'',`   ,':`   :''';`  .:''''',`  .:'''''', .:''''';.   `:'';.     .;'':`    .;''''.    .:'',`   :':`
+	   `'@;` .+@@+.   ;@'. `:#@@@#'. ,+@@@@@+;. ,+@@@@@@:`.+@@@@@@+.  ,+@@@:`    ;#@@+.  `:+#@@@@+;.  .+@@+.   '@'.
+	   `'@;` .+#@@:`  ;@'. :#@;,,;+, ,#@;,,+@#: ,#@',,,,. .+@',,;#@'. ,#@##'.   .+##@+.  :#@':,,+@@:` .+##@:`  ;@'.
+	   `'@;` .+#'+'.  ;@'. '@+.  `,` ,#@,  ,#@;`,#@:      .+@:  `'@#, ,#@'+#,   ,#++#+. .+@'.   .+@+. .+#'+'.  ;@'.
+	   `'@;` .+#,'#,  ;@'. '@#:`     ,#@,  .+@;`,#@,      .+@:   ;@#, ,#@:'@;`  ;@;'@+. ;##:     ;#@, .+#,'#,  ;@'.
+	   `'@;` .+#,,#'. ;@'. ,#@+':`   ,#@,  :#@: ,#@+''':` .+@:  .+@'. ,#@,;#+. `'#,'@+. '@+,     :#@: .+#,,#'. ;@'.
+	   `'@;` .+#,`'#, ;@'. `,#@@#'.  ,#@+''#@+. ,#@@@@@+. .+@+''+#+,  :#@,,+#, :#+.;@+. '@+.     ,#@: .+#,`'#, ;@'.
+	   `'@;` .+#, ,#'.;@'.  `.:+@@+. ,#@@@@@;.` ,#@;,,,.` .+@@@@@+,`  ,#@,`'@;`'@;`;@+. '@+.     ,#@: .+#, ,#'.;@'.
+	   `'@;` .+#, `'@,;@'.    `,'#@;`,#@;,,,`   ,#@,      .+@',;##:`  ,#@, ;@+:+@, '@+. '@#,     :#@: .+#, `'#,'@'.
+	   `'@;` .+#,  ,#;;#'.      .+@;`,#@,       ,#@,      .+@: `;@#,  ,#@, ,+@##'. '@+. ;#@,    `;@#, .+#,  ,#;;#'.
+	   `'@;` .+#,  `'#+#'.`:.   ,+@;`,#@,       ,#@,      .+@:  .+@;` ,#@, `'@@@:  ;@+. .+@+.   :#@;` .+#,  `'#+#'.
+	   `'@;` .+#,   ,#@@'.`'+'''+@+, ,#@,       ,#@+'''',`.+@:  `'@#, ,#@,  ;@@#,  ;@+.  :#@+'''#@'.  .+#,   ,##@'.
+	   `'@;` .+#,   `'@@;``,+@@@@;.` :#@,       ,+@@@@@@;`.+@:`  ,#@'`:#@,  ,#@'`  '@#,  `,+@@@@#;.   .+@,   `'@@'`
+	   `.,.  `,,`    .,,.   .:,,,`   `,,`       `,,,,,,,. `,,.   `,,,``,,`  `,,.   .,,`    .,,,,,`    `,,`    .,,.
+
+	  Bem vindos ao Inspermon! Um jogo completamente inovador, nunca visto antes, onde você anda por um mundo virtual e 
+	  batalha contra criaturas fantásticas pelo caminho (também conhecidas como alunos do Insper)!
+	  Siga as instruções para jogar.
+	  *** JOGUE COM O O CONSOLE EM TELA CHEIA ***
 """
 #INICIA O JOGO
 while 1:
@@ -39,10 +133,6 @@ while 1:
 		print("Vamos começar!")
 		time.sleep(2)
 		clear()
-		for i in range(5,0,-1):
-			print(i)
-			time.sleep(1)
-			clear()
 		break
 	else:
 		print("Você precisa digitar ou 0 ou 1!")
@@ -80,7 +170,12 @@ while 1:
 			time.sleep(3)
 
 	elif loadSaveOption == "n" or loadSaveOption == "N":
-		#PEGAR OS DADOS DO JOGADOR
+		clear()
+		for i in range(3,0,-1):
+			print(i)
+			time.sleep(1)
+			clear()
+		   #PEGAR OS DADOS DO JOGADOR
 		clear()
 		print("Bem vindo ao Kanto do Insper, o mundo de Inspermon!")
 		time.sleep(5)
@@ -95,10 +190,10 @@ while 1:
 		while 1:
 			name = input("Digite seu nome: ")
 			clear()
-			sex = int(input("Você é menino (0) ou menina (1)?" ))
-			if sex == 0:
+			sex = input("Você é menino (0) ou menina (1)? ")
+			if sex == "0":
 				sex = "Você é um menino"
-			elif sex == 1:
+			elif sex == "1":
 				sex = "Você é uma menina"
 			else:
 				print("Como você não digitou nem 0 nem 1, você não será considerado nem um menino, nem uma menina")
@@ -111,7 +206,7 @@ while 1:
 			clear()
 			if entrevistaConfirma == "S" or entrevistaConfirma=="s":
 				saveName = input("Escolha um nome para seu novo save: ")
-				saveGeneral[saveName] = {"name":name,"sex":sex}
+				saveGeneral[saveName] = {"name":name,"sex":sex,"alunos":[]}
 				saveAtual = saveGeneral[saveName]
 				with open("save.json","w") as arquivoSai:
 					json.dump(saveGeneral,arquivoSai)
@@ -123,11 +218,145 @@ while 1:
 
 		if entrevistaConfirma == "S" or entrevistaConfirma=="s":
 			break
-	
+
 	else:
 		print("Você precisa digitar (s) ou (n)!")
 		time.sleep(3)
 		clear()
 
-print(saveAtual)
-time.sleep(5)
+###########PROGRAMA PRINCIPAL#########################
+
+
+###INTRO - ESCOLHE O POKEMON INICIAL
+with open("save.json") as arquivoEntra:
+	saveGeneral = json.load(arquivoEntra)
+
+if loadSaveOption == "n" or loadSaveOption == "N":
+	clear()
+	print("Bem-vindo ao jardim do Kanto do Insper!")
+	time.sleep(3)
+	clear()
+	print("Aqui você encontrará criaturas às quais chamamos de Alunos. Eles são perigosos e poderosos!")
+	time.sleep(5)
+	clear()
+	print("Para te ajudar, tenho aqui 3 Alunos iniciais. Você pode escolher apenas um deles como seu Aluno inicial!")
+	time.sleep(5)
+	while 1:
+		print("Bixo de MECATRÔNICA (0)")
+		print("Bixo de MECÂNICA (1)")
+		print("Bixo de COMPUTAÇÃO (2)")
+		alunoInicialChoose = input("Qual deles você quer ter como seu inicial?")
+		clear()
+		if alunoInicialChoose == "0":
+			print("Você escolheu um aluno de MECATRÔNICA para ser seu aluno inicial! Estes são os stats dele:")
+			print("Ataque: {}".format(alunoMecatronica.ataque))
+			print("Defesa: {}".format(alunoMecatronica.defesa))
+			print("Vida: {}".format(alunoMecatronica.vida))
+			print("Level: {}".format(alunoMecatronica.lvl))
+			time.sleep(2)
+			alunoMecatronica.nome = input("Digite qual será o nome do seu Aluno inicial: ")
+			alunoInicial = alunoMecatronica
+			saveAtual["alunos"] = {}
+			saveAtual["alunos"] = {alunoMecatronica.nome:"alunoMecatronica"}
+			saveGeneral[saveName] = saveAtual
+			###COLOCA O ALUNO COM NOME NO SAVE
+			with open("save.json","w") as arquivoSai:
+				json.dump(saveGeneral,arquivoSai)
+			break
+
+		elif alunoInicialChoose == "1":
+			print("Você escolheu um aluno de MECÂNICA para ser seu aluno inicial! Estes são os stats dele:")
+			print("Ataque: {}".format(alunoMecanica.ataque))
+			print("Defesa: {}".format(alunoMecanica.defesa))
+			print("Vida: {}".format(alunoMecanica.vida))
+			print("Level: {}".format(alunoMecanica.lvl))
+			time.sleep(2)
+			alunoMecanica.nome = input("Digite qual será o nome do seu Aluno inicial: ")
+			alunoInicial = alunoMecanica
+			saveAtual["alunos"] = {}
+			saveAtual["alunos"] = {alunoMecanica.nome:"alunoMecanica"}
+			saveGeneral[saveName] = saveAtual
+			###COLOCA O ALUNO COM NOME NO SAVE
+			with open("save.json","w") as arquivoSai:
+				json.dump(saveGeneral,arquivoSai)
+			break
+
+		elif alunoInicialChoose == "2":
+			print("Você escolheu um aluno de COMPUTAÇÃO para ser seu aluno inicial! Estes são os stats dele:")
+			print("Ataque: {}".format(alunoComputacao.ataque))
+			print("Defesa: {}".format(alunoComputacao.defesa))
+			print("Vida: {}".format(alunoComputacao.vida))
+			print("Level: {}".format(alunoComputacao.lvl))
+			time.sleep(2)
+			alunoComputacao.nome = input("Digite qual será o nome do seu Aluno inicial: ")
+			alunoInicial = alunoComputacao
+			saveAtual["alunos"] = {}
+			saveAtual["alunos"] = {alunoComputacao.nome:"alunoComputacao"}
+			saveGeneral[saveName] = saveAtual
+			###COLOCA O ALUNO COM NOME NO SAVE
+			with open("save.json","w") as arquivoSai:
+				json.dump(saveGeneral,arquivoSai)
+			break
+		else:
+			print("Você precisa escolher qual Aluno inicial você quer!")
+			time.sleep(3)
+
+else:
+	#O saveAtual JA CARREGA OS VALORES DO SAVE
+	print("Bem-vindo de volta ao jardim do Kanto do Insper!")
+	time.sleep(3)
+
+
+while 1: #LOOP INFINITO PRINCIPAL DO JOGO
+	clear()
+	print("Estes são seus alunos:")
+	for i in saveAtual["alunos"].keys():
+		print(i)
+	alunoAtual = input("Digite com qual aluno você quer passear: ")
+	while 1: #LOOP DO PASSEAR OU DORMIR
+		clear()
+		if alunoAtual in saveAtual["alunos"]:	
+			clear()
+			print("Passeando com {}".format(alunoAtual))
+			passearOuDormir = input("Você quer passear (p) ou dormir (d) ? ") ###PASSEAR OU DORMIR
+			if passearOuDormir == "d" or passearOuDormir == "D":
+				break
+
+			elif passearOuDormir == "p" or passearOuDormir == "P":
+				clear()
+				print("Passeando")
+				time.sleep(1)
+				clear()
+				print("Passeando.")
+				time.sleep(1)
+				clear()
+				print("Passeando..")
+				time.sleep(1)
+				clear()
+				print("Passeando...")
+				time.sleep(1)
+				clear()
+				oponente = random.choice(listaNPC)
+				#CHAMA A FUNÇÃO BATALHAR
+				batalhar(saveAtual["alunos"][alunoAtual],oponente,alunoAtual)
+
+			else:
+				print("Você precisa digitar (p) para passear ou (d) para dormir!")
+				time.sleep(3)
+		else:
+			print("Escolha um Aluno que está na sua lista!")
+			time.sleep(2)
+			break	
+	clear()
+	if alunoAtual in saveAtual["alunos"]:
+		continuarOuSair = input("Você quer sair do jogo? Para sair digite (s). Para continuar aperte ENTER")
+		if continuarOuSair == "s" or continuarOuSair == "S":
+			clear()
+			print("Até a próxima!")
+			time.sleep(2)
+			exit()
+
+
+
+
+
